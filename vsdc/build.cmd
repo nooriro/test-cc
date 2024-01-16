52aa339323ee3893096635c805d15e6f80a87b48
nooriro <noorirogit@gmail.com>
2024-01-16 17:00:00 +0900

    Make VSDC build reproducible

--- vsdc/build.cmd
+++ vsdc/build.cmd
@@ -1,8 +1,10 @@
 @pushd %~dp0
 @setlocal
 @REM call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" x64 & echo on
+@REM call "%ProgramFiles%\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat" x64 & echo on
+@REM call "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" x64 & echo on
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" x64 & echo on
 rc manifest.rc
-cl /O2 /utf-8 nonbmp.c /link manifest.res
+cl /O2 /utf-8 /Brepro nonbmp.c /link manifest.res /Brepro
 @endlocal
 @popd
